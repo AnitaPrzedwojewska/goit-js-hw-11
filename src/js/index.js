18f12b9dde90ed64f055e5eef59e8aed8cb90e64
AnitaPrzedwojewska <nisiek@wp.pl>
2024-02-06 21:10:35 +0100

    Add modules

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.7.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
