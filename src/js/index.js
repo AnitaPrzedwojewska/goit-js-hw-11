3f3501c14d6f5bd806e60ea82b11b5514c01da35
AnitaPrzedwojewska <nisiek@wp.pl>
2024-02-09 22:02:54 +0100

    Added showMore

--- src/js/index.js
+++ src/js/index.js
@@ -12,12 +12,21 @@ const moreButton = document.querySelector('.load-more');
 
 moreButton.style.display = 'none';
 
-async function getImages(keywords, page) {
+const perPage = 40;
+let keywords = searchInput.value;
+let page = 1;
+let pages = 0;
+
+searchButton.addEventListener('click', showGallery);
+
+moreButton.addEventListener('click', showMore);
+
+async function getImages() {
   const apiKey = '42261128-30c11368cc5a6bd0852de3244';
 
   try {
     const response = await axios.get(
-      `https://pixabay.com/api/?key=${apiKey}&q=${keywords}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+      `https://pixabay.com/api/?key=${apiKey}&q=${keywords}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
     );
     return response.data;
   } catch {
@@ -25,33 +34,8 @@ async function getImages(keywords, page) {
   }
 }
 
-function showImages(result) {
-  console.log('result: ', result);
-  const hits = result.totalHits;
-  console.log('hits: ', hits);
-  const images = result.hits.map(
-    ({
-      webformatURL,
-      largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    }) => ({
-      webformatURL,
-      largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    })
-  );
-  console.log('images: ', images);
-  // result2.unschift({ result.totalHits });
-  // console.log(result2);
-  const galleryImages = images.map(
+const showImages = (result) => {
+  const galleryImages = result.hits.map(
     ({
       webformatURL,
       largeImageURL,
@@ -80,24 +64,42 @@ function showImages(result) {
   </div>`
   );
   const galleryAll = galleryImages.join('');
-  galleryElement.innerHTML = ``;
   galleryElement.insertAdjacentHTML('beforeend', galleryAll);
-  moreButton.style.display = 'block';
 }
 
 async function showGallery(event) {
   event.preventDefault();
   const searchKeywords = searchInput.value;
-  const keywords = searchKeywords.split(' ').join('+');
+  keywords = searchKeywords.split(' ').join('+');
+  galleryElement.innerHTML = ``;
   getImages(keywords, 1)
-  .then(result => showImages(result))
-  .catch(error => Notiflix.Notify.failure(error));
+    .then(result => {
+      const hits = result.totalHits;
+      if (!hits) {
+        Promise.reject(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      pages = Math.floor(hits / 40) + (hits % 40 ? 1 : 0);
+      console.log('pages: ', pages);
+      showImages(result);
+      if (pages > 1) {
+        moreButton.style.display = 'block';
+      }
+    })
+    .catch(error => Notiflix.Notify.failure(error));
 }
-
-searchButton.addEventListener('click', showGallery);
 
 function showMore() {
-  
+  // debugger;
+  page++;
+  getImages()
+    .then(result => {
+      showImages(result);
+      console.log(`Page ${page} of ${pages} pages`);
+      if ((page === pages)) {
+        moreButton.style.display = 'none';
+      }
+    })
+    .catch(error => Notiflix.Notify.failure(error));
 }
-
-moreButton.addEventListener('click', showMore);
